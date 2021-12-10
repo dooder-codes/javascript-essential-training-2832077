cac6a8280f6be005b5f6a5fa91e11a4b6de80a10
Saavv <skyviewsmith@gmail.com>
2021-12-09 19:05:22 -0500

    December 9, 2021 7:05 PM

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,15 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const Book = {
+  title: "How to Code Your Own Software",
+    Release_Date: 2022,
+  topics: [],
+  author: {
+    name: "Michael Smith",
+    Age: 22,
+    Gender: "Male",
+  },
+};
+Book.top
