f2e395903efe2c767fe59580ba539e3c43d213b2
Saavv <skyviewsmith@gmail.com>
2021-12-09 19:47:13 -0500

    December 9, 2021 7:47 PM

---
+++ Practice/home/index.js
@@ -0,0 +1,14 @@
+function objectConstructor(day, month, year) {
+  this.day = day;
+  this.month = month;
+  this.year = year;
+  this.changeDate = function (day, month, year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  };
+  this.printDate = function () {
+    console.log(this.date, this.month, this.year);
+  };
+}
+const newDate = new objectConstructor(9, 12, 2021);
